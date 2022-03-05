83f84c94ff0b79bb625088a2514b6a398967c9b2
dennysferrer <dennys.ferrer@gmail.com>
2022-03-05 18:28:49 -0500

    Se adicionó el capitulo de listas, donde se repasan todos los conceptos del tema

---
+++ 09-listas/lista.py
@@ -0,0 +1,11 @@
+peliculas = ["Matrix","El señor de los anillos","El Hobbit","Star trek","Star Wars"]
+
+nueva_pelicula = ""
+
+while(nueva_pelicula != 'parar'):
+    nueva_pelicula = input('Digite el nombre de una pelicula: ')
+    peliculas.append(nueva_pelicula)
+
+print(peliculas)
+
+
