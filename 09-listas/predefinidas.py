0e9ab6c595f37b060c5b17e3935e12e1046c6f88
dennysferrer <dennys.ferrer@gmail.com>
2022-03-12 16:17:57 -0500

    En el archivo predefinidos.py se trabajaron algunos metodos para las listas y a continuación se iniciará con el nuevo capitulo correspondiente a sets y diccionarios

--- 09-listas/predefinidas.py
+++ 09-listas/predefinidas.py
@@ -1,1 +1,40 @@
-cantantes = []
+cantantes = ['2pac', 'Drake', 'Bad Bunny', 'Julio Iglesias']
+numeros = [1,2,5,8,3,4]
+
+print(numeros)
+numeros.sort()
+print(numeros)
+
+# Añadir elementos
+
+cantantes.append('Sylvestre Dangond')
+cantantes.insert(1,'David Bisbal')
+print(cantantes)
+
+# Eliminar elementos
+
+cantantes.pop(1)
+print(cantantes)
+
+cantantes.remove('Bad Bunny')
+print(cantantes)
+
+# Dar la vuelta a la lista
+
+numeros.reverse()
+print(numeros)
+
+#Buscar elementos dentro de la lista
+
+print('Drake' in cantantes)
+print(len(cantantes))
+
+#Cuantas veces aparece un elemento en la lista
+
+print(numeros.count(8))
+print(cantantes.index('Drake'))
+
+#Unir listas
+
+cantantes.extend(numeros)
+print(cantantes)
