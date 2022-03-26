b972e709f8f9bc9d0f8663bd7ce2725d48ce042b
dennysferrer <dennys.ferrer@gmail.com>
2022-03-26 17:33:13 -0500

    Actualizando archivos de paquetes

---
+++ 13-paquetes/main.py
@@ -0,0 +1,7 @@
+from mipaquete import pruebas
+from mipaquete import herramientas
+
+print("Probando paquetes: ")
+
+pruebas.probando()
+herramientas.nombreCompleto("Dennys", "Ferrer")
---
+++ 13-paquetes/mipaquete/pruebas.py
@@ -0,0 +1,4 @@
+def probando():
+    print("Esto es una prueba de un módulo dentro de un paquete")
+
+    
